04f8d15ad5832f31f523636c8acc1744b90c258e
Conch <unsoughtconch@gmail.com>
2024-01-24 20:48:43 +0000

    i forgor

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -6,10 +6,21 @@ class TransactionsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      HomeCard(title: "Amount Spent Today", content: "\$200"),
-      Text("Transactions", style: Theme.of(context).textTheme.titleLarge),
-      Expanded(child: TransactionList()),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch, 
+      children: [
+        HomeCard(title: "Amount Spent Today", content: "\$200"),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(2, 16, 2, 4),
+                child: Text("Transactions", style: Theme.of(context).textTheme.headlineMedium),
+              ),
+              Expanded(child: TransactionList()),
+            ],
+          ),
+        ),
     ]);
   }
 }
@@ -20,24 +31,31 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: ListTile(
-            leading: const Icon(Icons.monetization_on),
-            title: Text("Transaction $index"),
-            subtitle: Text("Transaction $index"),
-            trailing: const Icon(Icons.more_vert),
-            tileColor: theme.colorScheme.secondaryContainer,
-            textColor: theme.colorScheme.onSecondaryContainer,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ListView.builder(
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Card(
+              child: ListTile(
+                leading: const Icon(Icons.monetization_on),
+                title: Text("Transaction $index"),
+                subtitle: Text("Transaction $index"),
+                trailing: const Icon(Icons.more_vert),
+                tileColor: theme.colorScheme.secondaryContainer,
+                textColor: theme.colorScheme.onSecondaryContainer,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
