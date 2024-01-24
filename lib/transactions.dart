3ce43dd1b2d407ac0229f1ffba9450578cab841c
Conch <unsoughtconch@gmail.com>
2024-01-24 03:30:09 +0000

    Begin adding list of transactions

--- lib/transactions.dart
+++ lib/transactions.dart
@@ -6,10 +6,38 @@ class TransactionsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: const [
-      SizedBox(
-          height: 200,
-          child: HomeCard(title: "Amount Spent Today", content: "\$200")),
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        HomeCard(title: "Amount Spent Today", content: "\$200"),
+        TransactionList(),
     ]);
+  }
+}
+
+class TransactionList extends StatelessWidget {
+  const TransactionList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Column(
+        children: [
+          Text("Transactions", style: Theme.of(context).textTheme.titleLarge),
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return ListTile(
+                leading: const Icon(Icons.monetization_on),
+                title: Text("Transaction $index"),
+                subtitle: Text("Transaction $index"),
+                trailing: const Icon(Icons.more_vert),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
