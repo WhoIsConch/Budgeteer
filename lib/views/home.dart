e9258c49b2eb1de7f08ec0f3b548ff3af66d748a
WhoIsConch <conch@twisea.net>
2025-04-26 20:19:10 -0400

    Constrain Recent Activity more

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:budget/models/filters.dart';
 import 'package:budget/services/app_database.dart';
 import 'package:budget/views/components/accounts_carousel.dart';
@@ -87,20 +88,21 @@ class TransactionPreviewer extends StatelessWidget {
     final TransactionDao dao = context.watch<TransactionDao>();
 
     return SizedBox(
-      height: 160,
-      child: StreamBuilder(
+      height: 150,
+      child: StreamBuilder<List<Transaction>>(
           stream: dao.watchTransactionsPage(filters: [
             TransactionFilter(DateTimeRange(
-                start: DateTime.now().subtract(Duration(days: 7)),
-                end: DateTime.now()))
+                    start: DateTime.now().subtract(Duration(days: 7)),
+                    end: DateTime.now())
+                .makeInclusive())
           ]),
           builder: (context, snapshot) {
-            if (!snapshot.hasData) {
+            if (!snapshot.hasData ||
+                snapshot.hasData && snapshot.data!.isEmpty) {
               return Center(child: CircularProgressIndicator());
             }
 
             return ListView.builder(
-              shrinkWrap: true,
               scrollDirection: Axis.horizontal,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) =>
@@ -119,37 +121,47 @@ class TransactionPreviewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
-    return Card(
-      color: Theme.of(context).colorScheme.tertiaryContainer,
-      child: InkWell(
-        borderRadius: BorderRadius.circular(12), // To match the card's radius
-        onTap: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (_) => ManageTransactionDialog(
-                  mode: ObjectManageMode.edit, transaction: transaction)));
-        },
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Expense",
-                    style: theme.textTheme.titleMedium!.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: theme.colorScheme.onTertiaryContainer)),
-                Text("\$${formatAmount(transaction.amount)}",
-                    style: theme.textTheme.headlineLarge!.copyWith(
-                        fontWeight: FontWeight.bold,
-                        color: theme.colorScheme.onTertiaryContainer)),
-                Text(transaction.title,
-                    style: theme.textTheme.titleMedium!.copyWith(
-                        color: theme.colorScheme.onTertiaryContainer)),
-                Text(DateFormat('M/d/yy').format(transaction.date),
-                    style: theme.textTheme.bodyLarge!.copyWith(
-                        color: theme.colorScheme.onTertiaryContainer
-                            .withAlpha(150)))
-              ]),
+    return SizedBox(
+      height: 150,
+      width: 150,
+      child: Card(
+        color: Theme.of(context).colorScheme.tertiaryContainer,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(12), // To match the card's radius
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(
+                builder: (_) => ManageTransactionDialog(
+                    mode: ObjectManageMode.edit, transaction: transaction)));
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                      transaction.type == TransactionType.expense
+                          ? "Expense"
+                          : "Category",
+                      style: theme.textTheme.titleMedium!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: theme.colorScheme.onTertiaryContainer)),
+                  AutoSizeText("\$${formatAmount(transaction.amount)}",
+                      maxLines: 1,
+                      style: theme.textTheme.headlineLarge!.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: theme.colorScheme.onTertiaryContainer)),
+                  Text(transaction.title,
+                      style: theme.textTheme.titleMedium!.copyWith(
+                          color: theme.colorScheme.onTertiaryContainer),
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis),
+                  Text(DateFormat('M/d/yy').format(transaction.date),
+                      style: theme.textTheme.bodyLarge!.copyWith(
+                          color: theme.colorScheme.onTertiaryContainer
+                              .withAlpha(150)))
+                ]),
+          ),
         ),
       ),
     );
